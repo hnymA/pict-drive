e35699ea2df647073bc566213d12f8882f072c9e
hnymA <hnyma637111@gmail.com>
2018-12-26 12:41:35 +0900

    [tmp] work in progress

---
+++ Assets/Scripts/CoursesScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoursesScript : MonoBehaviour
+{
+    public Material courseMaterial;
+
+    void Start()
+    {
+        int width = Screen.width;
+        int height = Screen.height;
+
+        LineRenderer renderer = gameObject.GetComponent<LineRenderer>();
+        renderer.material = courseMaterial;
+        renderer.SetWidth(0.1f, 0.1f);
+        renderer.SetVertexCount(3);
+
+        // TODO: adjust size according to screen resolution
+        switch (this.name)
+        {
+            case "LeftTopCourse":
+                setLeftTopPosition(renderer);
+                break;
+            case "LeftBottomCourse":
+                setLeftBottomPosition(renderer);
+                break;
+            case "RightTopCourse":
+                setRightTopPosition(renderer);
+                break;
+            case "RightBottomCourse":
+                setRightBottomPosition(renderer);
+                break;
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    void setLeftTopPosition(LineRenderer renderer)
+    {
+        renderer.SetPosition(0, new Vector3(-8f, 1.5f, -5.0f));
+        renderer.SetPosition(1, new Vector3(-1.5f, 1.5f, -5.0f));
+        renderer.SetPosition(2, new Vector3(-1.5f, 5f, -5.0f));
+    }
+
+    void setLeftBottomPosition(LineRenderer renderer)
+    {
+        renderer.SetPosition(0, new Vector3(-8f, -1.5f, -5.0f));
+        renderer.SetPosition(1, new Vector3(-1.5f, -1.5f, -5.0f));
+        renderer.SetPosition(2, new Vector3(-1.5f, -5f, -5.0f));
+    }
+    
+    void setRightTopPosition(LineRenderer renderer)
+    {
+        renderer.SetPosition(0, new Vector3(8f, 1.5f, -5.0f));
+        renderer.SetPosition(1, new Vector3(1.5f, 1.5f, -5.0f));
+        renderer.SetPosition(2, new Vector3(1.5f, 5f, -5.0f));
+    }
+    
+    void setRightBottomPosition(LineRenderer renderer)
+    {
+        renderer.SetPosition(0, new Vector3(8f, -1.5f, -5.0f));
+        renderer.SetPosition(1, new Vector3(1.5f, -1.5f, -5.0f));
+        renderer.SetPosition(2, new Vector3(1.5f, -5f, -5.0f));
+    }
+}
---
+++ Assets/Scripts/StopLinesScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopLinesScript : MonoBehaviour
+{
+    public Material stopLineMaterial;
+
+    void Start()
+    {
+        int width = Screen.width;
+        int height = Screen.height;
+
+        LineRenderer renderer = gameObject.GetComponent<LineRenderer>();
+        renderer.material = stopLineMaterial;
+        renderer.SetWidth(0.1f, 0.1f);
+        renderer.SetVertexCount(2);
+
+        // TODO: adjust size according to screen resolution
+        switch (this.name)
+        {
+            case "LeftTopStopLine":
+                setLeftTopPosition(renderer);
+                break;
+            case "LeftBottomStopLine":
+                setLeftBottomPosition(renderer);
+                break;
+            case "RightTopStopLine":
+                setRightTopPosition(renderer);
+                break;
+            case "RightBottomStopLine":
+                setRightBottomPosition(renderer);
+                break;
+        }
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void setLeftTopPosition(LineRenderer renderer)
+    {
+        renderer.SetPosition(0, new Vector3(-2.3f, 1.5f, -5.0f));
+        renderer.SetPosition(1, new Vector3(-2.3f, 0.2f, -5.0f));
+    }
+
+    void setLeftBottomPosition(LineRenderer renderer)
+    {
+        renderer.SetPosition(0, new Vector3(-1.5f, -2.3f, -5.0f));
+        renderer.SetPosition(1, new Vector3(-0.3f, -2.3f, -5.0f));
+    }
+
+    void setRightTopPosition(LineRenderer renderer)
+    {
+        renderer.SetPosition(0, new Vector3(1.5f, 2.3f, -5.0f));
+        renderer.SetPosition(1, new Vector3(0.3f, 2.3f, -5.0f));
+    }
+
+    void setRightBottomPosition(LineRenderer renderer)
+    {
+        renderer.SetPosition(0, new Vector3(2.3f, -1.5f, -5.0f));
+        renderer.SetPosition(1, new Vector3(2.3f, -0.2f, -5.0f));
+    }
+}
